a266d07fb45a8e5f3a882553840b6d72fcba1008
Bahadır Araz <bahadiraraz@protonmail.com>
2024-05-07 16:00:15 +0300

    2.1.6 version

--- exponent_server_sdk_async/main.py
+++ exponent_server_sdk_async/main.py
@@ -1,52 +1,71 @@
+import asyncio
 import os
 import httpx
-from httpx import HTTPError, NetworkError
 from loguru import logger
 from dotenv import load_dotenv
 from exponent_server_sdk_async import (
-    DeviceNotRegisteredError,
     AsyncPushClient,
     PushMessage,
-    PushServerError,
+    DeviceNotRegisteredError,
     PushTicketError,
+    PushServerError
 )
 
+# Logger setup
 logger.add("debug.log", rotation="1 week", compression="zip")
 load_dotenv()
 logger.debug("Using Expo Token: {}", os.getenv('EXPO_TOKEN'))
 
 
-async def send_push_message(token, message, extra=None):
+async def send_push_message_to_multiple(tokens, message, extra, title, navigate_to=None):
     async with httpx.AsyncClient(headers={
         "Authorization": f"Bearer {os.getenv('EXPO_TOKEN')}",
         "Accept": "application/json",
         "Content-Type": "application/json",
     }) as session:
+        push_client = AsyncPushClient(session=session)
+        push_messages = [
+            PushMessage(
+                to=token,
+                body=message,
+                data={'extra': extra, 'navigateTo': navigate_to},
+                badge=1,
+                title=title,
+                sound="default"
+            ) for token in tokens
+        ]
         try:
-            response = await AsyncPushClient(session=session).publish(
-                PushMessage(to=token, body=message, data=extra, sound="default"))
-            response.validate_response()
+            push_tickets = await push_client.publish_multiple(push_messages)
+            for push_ticket in push_tickets:
+                if push_ticket.is_success():
+                    logger.info("Notification sent successfully to: {}", push_ticket.push_message.to)
+                else:
+                    logger.warning("Failed to send notification to: {}, Error: {}", push_ticket.push_message.to, push_ticket.message)
         except PushServerError as exc:
             logger.error("Push server error: {}", exc)
             raise
-        except (NetworkError, HTTPError) as exc:
-            logger.error("Network or HTTP error: {}", exc)
+        except DeviceNotRegisteredError as exc:
+            logger.error("Device not registered error: {}", exc)
             raise
-        except DeviceNotRegisteredError:
-            logger.warning("Device not registered, deactivating token: {}", token)
-
         except PushTicketError as exc:
             logger.error("Push ticket error: {}", exc)
             raise
 
+
 async def main():
-    token = "ExponentPushToken[TOKEN]"
-    message = "HI! This is a test message."
+    tokens = [
+        "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]",
+        "ExponentPushToken[yyyyyyyyyyyyyyyyyyyyyy]",
+        "ExponentPushToken[zzzzzzzzzzzzzzzzzzzzzz]"
+    ]
     extra_parameters = {"key": "value"}
-    await send_push_message(token, message, extra_parameters)
+
+    # Send a simple push message to multiple recipients
+    await send_push_message_to_multiple(tokens, "Hello Multi-User", extra_parameters, "Greeting")
+
+    # Navigate to home data for multiple recipients
+    await send_push_message_to_multiple(tokens, "Navigate to home", extra_parameters, "Home", "/(tabs)/(home)")
 
 
 if __name__ == "__main__":
-    import asyncio
-
     asyncio.run(main())
